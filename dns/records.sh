e420de785e111d456b5c33c96348e30e1336e5af
gzalz <eric.gonzalez@jito.wtf>
2026-04-03 17:26:51 -0600

    dns records script

---
+++ dns/records.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+regions=$(curl -s https://raw.githubusercontent.com/jito-foundation/bam-plugins/refs/heads/feat/testnet-regions/data/testnet-regions.txt)
+for region in $regions; do
+    nslookup "$region.testnet.bam.jito.wtf" 2>/dev/null | awk '/^Address/ && !/#/ {
+        ip = $2
+        if (ip !~ /^(10\.|172\.(1[6-9]|2[0-9]|3[01])\.|192\.168\.|127\.|169\.254\.)/) print ip ":5012"
+    }'
+done
